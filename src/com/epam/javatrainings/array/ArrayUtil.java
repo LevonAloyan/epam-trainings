2a96f947a4465f7e08212aab195991d60ba0efa7
Levon_Aloyan <>
2021-08-24 18:26:37 +0400

    Added ArrayUtil homework

---
+++ src/com/epam/javatrainings/array/ArrayUtil.java
@@ -0,0 +1,64 @@
+package com.epam.javatrainings.array;
+
+public class ArrayUtil {
+
+    /**
+     * Sorts the array elements by odd and even number
+     * such that if first number is odd then second must be even number
+     * this means that it will be found the next even number and swap with the second element
+     * if the second element is odd. And so on for the next elements of the array
+     *
+     */
+    public void sortByOddAndEven(int[] array) {
+
+    }
+
+    /**
+     * Return maximum element from given array
+     */
+    public static int getMaximum(int[] array) {
+
+        return 0;
+    }
+
+    /**
+     * Return minimum element from given array
+     */
+    public static int getMinimum(int[] array) {
+
+        return 0;
+    }
+
+    /**
+     * Calculates the sum of specified array  elements
+     *
+     * @param array specified int array
+     * @return the sum of the specified array elements
+     */
+    public static int getSum(int[] array) {
+
+        return 0;
+    }
+
+    /**
+     * Revers given array
+     * Example:
+     * Given array [2,5,7,3,2,6]
+     * Expected array [6,2,3,7,5,2]
+     */
+    public static int[] reverse(int[] array) {
+
+        return null;
+    }
+
+    /**
+     * Prints all elements in an array.
+     *
+     * @param array the array to print
+     */
+    public static void print(int[] array) {
+
+    }
+
+
+}
---
+++ src/com/epam/javatrainings/array/ArrayUtilTest.java
@@ -0,0 +1,8 @@
+package com.epam.javatrainings.array;
+
+public class ArrayUtilTest {
+
+    public static void main(String[] args) {
+
+    }
+}
