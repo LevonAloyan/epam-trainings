f4f6d8d0d69e27e14318a18a9adba702e85731f6
mush-yegh <mush.yegh@gmail.com>
2021-08-26 19:12:51 +0400

    Implement array methods

--- src/com/epam/javatrainings/array/ArrayUtil.java
+++ src/com/epam/javatrainings/array/ArrayUtil.java
@@ -1,5 +1,7 @@
 package com.epam.javatrainings.array;
 
+import java.util.Arrays;
+
 public class ArrayUtil {
 
     /**
@@ -7,26 +9,80 @@ public class ArrayUtil {
      * such that if first number is odd then second must be even number
      * this means that it will be found the next even number and swap with the second element
      * if the second element is odd. And so on for the next elements of the array
-     *
      */
     public void sortByOddAndEven(int[] array) {
+        if (array == null) {
+            throw new NullPointerException("Parameter 'array' cannot be null");
+        }
 
+        int len = array.length;
+
+        if (len == 0) {
+            ArrayUtil.print(array);
+            return;
+        }
+
+        boolean isFirstNumEven = array[0] % 2 == 0;
+        int[] source = Arrays.stream(array)
+                .filter(n -> isFirstNumEven ? n % 2 == 0 : n % 2 != 0)
+                .toArray();
+
+        int[] arrayToMerge = Arrays.stream(array)
+                .filter(n -> isFirstNumEven ? n % 2 != 0 : n % 2 == 0)
+                .toArray();
+
+        int[] result = new int[len];
+        int i = 0, j = 0, k = 0;
+
+        // Merge 'source' and 'arrayToMerge' into 'result'
+        while (k < len) {
+            if (i < source.length) {
+                result[k++] = source[i++];
+            }
+            if (j < arrayToMerge.length) {
+                result[k++] = arrayToMerge[j++];
+            }
+        }
+
+        ArrayUtil.print(result);
     }
 
     /**
      * Return maximum element from given array
      */
     public static int getMaximum(int[] array) {
+        if (array == null) {
+            throw new NullPointerException("Parameter 'array' cannot be null");
+        }
 
-        return 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int val : array) {
+            if (val > max) {
+                max = val;
+            }
+        }
+
+        return max;
     }
 
     /**
      * Return minimum element from given array
      */
     public static int getMinimum(int[] array) {
+        if (array == null) {
+            throw new NullPointerException("Parameter 'array' cannot be null");
+        }
 
-        return 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int val : array) {
+            if (val < min) {
+                min = val;
+            }
+        }
+
+        return min;
     }
 
     /**
@@ -36,8 +92,11 @@ public class ArrayUtil {
      * @return the sum of the specified array elements
      */
     public static int getSum(int[] array) {
+        if (array == null) {
+            throw new NullPointerException("Parameter 'array' cannot be null");
+        }
 
-        return 0;
+        return Arrays.stream(array).reduce(0, Integer::sum);
     }
 
     /**
@@ -47,8 +106,18 @@ public class ArrayUtil {
      * Expected array [6,2,3,7,5,2]
      */
     public static int[] reverse(int[] array) {
+        if (array == null) {
+            throw new NullPointerException("Parameter 'array' cannot be null");
+        }
 
-        return null;
+        int len = array.length;
+        int[] result = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            result[i] = array[len - 1 - i];
+        }
+
+        return result;
     }
 
     /**
@@ -57,7 +126,23 @@ public class ArrayUtil {
      * @param array the array to print
      */
     public static void print(int[] array) {
+        if (array == null) {
+            throw new NullPointerException("Parameter 'array' cannot be null");
+        }
+        if (array.length == 0) {
+            System.out.print("[]");
+            return;
+        }
 
+        String delimiter = ", ";
+        StringBuilder result = new StringBuilder("[");
+
+        for (int num : array) {
+            result.append(num).append(delimiter);
+        }
+
+        result.replace(result.length() - 2, result.length(), "]");
+        System.out.print(result);
     }
 
 
--- src/com/epam/javatrainings/array/ArrayUtilTest.java
+++ src/com/epam/javatrainings/array/ArrayUtilTest.java
@@ -1,8 +1,23 @@
 package com.epam.javatrainings.array;
 
+import java.util.Random;
+
 public class ArrayUtilTest {
 
     public static void main(String[] args) {
+        int[] randomInts = new Random().ints(9, Integer.MIN_VALUE, Integer.MAX_VALUE).toArray();
 
+        System.out.print("Original array: ");
+        ArrayUtil.print(randomInts);
+
+        System.out.print("\nSorted: \t\t");
+        new ArrayUtil().sortByOddAndEven(randomInts);
+
+        System.out.println("\nGet max: \t\t" + ArrayUtil.getMaximum(randomInts));
+        System.out.println("Get min: \t\t" + ArrayUtil.getMinimum(randomInts));
+        System.out.println("Get sum: \t\t" + ArrayUtil.getSum(randomInts));
+
+        System.out.print("Reversed:\t\t");
+        ArrayUtil.print(ArrayUtil.reverse(randomInts));
     }
 }
