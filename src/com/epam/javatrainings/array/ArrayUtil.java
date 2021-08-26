c343bc13359c07919b5a4c664738302d254c4fc2
ErikMargaryan <erickmargarian@gmail.com>
2021-08-26 05:07:36 +0400

    Homework 1

--- src/com/epam/javatrainings/array/ArrayUtil.java
+++ src/com/epam/javatrainings/array/ArrayUtil.java
@@ -1,5 +1,8 @@
 package com.epam.javatrainings.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayUtil {
 
     /**
@@ -7,26 +10,60 @@ public class ArrayUtil {
      * such that if first number is odd then second must be even number
      * this means that it will be found the next even number and swap with the second element
      * if the second element is odd. And so on for the next elements of the array
-     *
      */
     public void sortByOddAndEven(int[] array) {
+        var oddElements = new ArrayList<>();
+        var evenElements = new ArrayList<>();
+        var arrayElements = new ArrayList<>();
+        var sortedElementsEvenByOdd = new ArrayList<>();
+        var sortedElementsOddByEven = new ArrayList<>();
+        for (int i : array) {
+            if (i % 2 == 0) {
+                evenElements.add(i);
+            } else oddElements.add(i);
+        }
 
+        arrayElements.addAll(evenElements);
+        arrayElements.addAll(oddElements);
+
+        //even -> odd
+        for (int i = 0, j = 0; i < evenElements.size() && j < oddElements.size(); i++, j++) {
+            sortedElementsEvenByOdd.add(evenElements.get(i));
+            sortedElementsEvenByOdd.add(oddElements.get(j));
+        }
+
+        //odd -> even
+        for (int i = 0, j = 0; i < evenElements.size() && j < oddElements.size(); i++, j++) {
+            sortedElementsOddByEven.add(oddElements.get(j));
+            sortedElementsOddByEven.add(evenElements.get(i));
+        }
+        System.out.println("Sort even by odd: " + sortedElementsEvenByOdd);
+        System.out.println("Sort odd by even: " + sortedElementsOddByEven);
     }
 
     /**
      * Return maximum element from given array
      */
     public static int getMaximum(int[] array) {
-
-        return 0;
+        int max = array[0];
+        for (int i : array) {
+            if (i > max)
+                max = i;
+        }
+        return max;
     }
 
     /**
      * Return minimum element from given array
      */
     public static int getMinimum(int[] array) {
-
-        return 0;
+        int min = array[0];
+        for (int i : array) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
 
     /**
@@ -36,8 +73,11 @@ public class ArrayUtil {
      * @return the sum of the specified array elements
      */
     public static int getSum(int[] array) {
-
-        return 0;
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -47,8 +87,13 @@ public class ArrayUtil {
      * Expected array [6,2,3,7,5,2]
      */
     public static int[] reverse(int[] array) {
-
-        return null;
+        var newArray = new int[array.length];
+        int j = array.length;
+        for (int k : array) {
+            newArray[j - 1] = k;
+            j = j - 1;
+        }
+        return newArray;
     }
 
     /**
@@ -57,7 +102,12 @@ public class ArrayUtil {
      * @param array the array to print
      */
     public static void print(int[] array) {
-
+        //variant 1
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        //variant 2
+        System.out.println(Arrays.toString(array));
     }
 
 
--- src/com/epam/javatrainings/array/ArrayUtilTest.java
+++ src/com/epam/javatrainings/array/ArrayUtilTest.java
@@ -1,8 +1,17 @@
 package com.epam.javatrainings.array;
 
+import java.util.Arrays;
+
 public class ArrayUtilTest {
 
     public static void main(String[] args) {
-
+        var array = new int[]{2, 5, 7, 3, 2, 6}; //or var -> int[]
+        System.out.print("Print array: "); ArrayUtil.print(array);
+        System.out.println("Reverse: " + Arrays.toString(ArrayUtil.reverse(array)));
+        System.out.println("Sum: " + ArrayUtil.getSum(array));
+        System.out.println("Get min: " + ArrayUtil.getMinimum(array));
+        System.out.println("Get max: " + ArrayUtil.getMaximum(array));
+        ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.sortByOddAndEven(array);
     }
 }
