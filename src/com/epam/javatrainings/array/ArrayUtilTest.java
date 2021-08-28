ce9f262a536f6e4b3d46514451d7c4f446b809b5
Admin <mkrtart9211@gmail.com>
2021-08-28 12:38:35 +0400

    Homework No1

--- src/com/epam/javatrainings/array/ArrayUtilTest.java
+++ src/com/epam/javatrainings/array/ArrayUtilTest.java
@@ -1,8 +1,21 @@
 package com.epam.javatrainings.array;
 
+/**
+ * @author Artur Mkrtumyan
+ */
 public class ArrayUtilTest {
 
     public static void main(String[] args) {
-
+        int[] arr = new int[]{1,3,7,10,1};
+        //Test method getMinimum
+        System.out.println(ArrayUtil.getMinimum(arr));
+        //Test method getMaximum
+        System.out.println(ArrayUtil.getMaximum(arr));
+        //Test method reverse and print
+        ArrayUtil.print(ArrayUtil.reverse(arr));
+        System.out.println();
+        //Test method sortByOddAndEven
+        ArrayUtil.sortByOddAndEven(arr);
+        ArrayUtil.print(arr);
     }
 }
