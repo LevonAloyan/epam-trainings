8bd63ccbc470945ad5cf92fe7cae74dff459fb6f
Sargis Sargsyan <sargiss987@gmail.com>
2021-08-31 21:42:19 +0400

    commit 2

--- src/com/epam/javatrainings/array/exeption/EmptyArrayException.java
+++ src/com/epam/javatrainings/array/exeption/EmptyArrayException.java
@@ -5,7 +5,7 @@ package com.epam.javatrainings.array.exeption;
 public class EmptyArrayException extends Exception{
 
 
-    private static final String errorMassage = "The array is empty";
+    private static final String errorMassage = "The array must noy be empty";
 
     public EmptyArrayException() {
         super(errorMassage);
