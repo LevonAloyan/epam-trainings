19840a8a26205bfae7609f6759eae743ab6246f2
Anna <anya.qaramyan00@mail.ru>
2021-09-03 14:53:20 -0700

    :)

--- src/com/epam/javatrainings/classdecomposition/model/Verification.java
+++
@@ -1,8 +0,0 @@
-package com.epam.javatrainings.classdecomposition.model;
-
-public class Verification {
-   /* public static boolean pizzaName(String name, int numberOfCustomer){
-       return true;
-
-    } return false;*/
-}
