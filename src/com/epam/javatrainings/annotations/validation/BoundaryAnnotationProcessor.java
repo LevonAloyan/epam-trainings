38034a61a3ac248602eccb87a69e06442ddd746e
Ara Gasparyan <ara.gasparyan87@gmail.com>
2021-09-08 16:46:04 +0400

    Remove unnessacary method declaration

--- src/com/epam/javatrainings/annotations/validation/BoundaryAnnotationProcessor.java
+++ src/com/epam/javatrainings/annotations/validation/BoundaryAnnotationProcessor.java
@@ -1,9 +1,6 @@
 package com.epam.javatrainings.annotations.validation;
 
-import java.lang.annotation.Annotation;
-
 public abstract class BoundaryAnnotationProcessor extends AnnotationProcessor {
-    abstract String validate(Annotation annotation, Object value);
 
     protected double getDoubleValue(Number n) {
         double dValue = 0;
