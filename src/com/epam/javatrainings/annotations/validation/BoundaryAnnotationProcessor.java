aefd1f8cb575267617cac196c97f06d821c2bc01
Ara Gasparyan <ara.gasparyan87@gmail.com>
2021-09-09 01:30:10 +0400

    Simplify implementation of the method getDoubleValue

--- src/com/epam/javatrainings/annotations/validation/BoundaryAnnotationProcessor.java
+++ src/com/epam/javatrainings/annotations/validation/BoundaryAnnotationProcessor.java
@@ -3,22 +3,6 @@ package com.epam.javatrainings.annotations.validation;
 public abstract class BoundaryAnnotationProcessor extends AnnotationProcessor {
 
     protected double getDoubleValue(Number n) {
-        double dValue = 0;
-
-        if (n instanceof Byte) {
-            dValue = (Byte) n;
-        } else if (n instanceof Short) {
-            dValue = (Short) n;
-        } else if (n instanceof Integer) {
-            dValue = (Integer) n;
-        } else if (n instanceof Long) {
-            dValue = (Long) n;
-        } else if (n instanceof Float) {
-            dValue = (Float) n;
-        } else if (n instanceof Double) {
-            dValue = (Double) n;
-        }
-
-        return dValue;
+        return n.doubleValue();
     }
 }
