18fe1da050cd07288fe06a1b42e1e8a5afaf3bd7
Admin <mkrtart9211@gmail.com>
2021-09-12 14:20:11 +0400

    thread after changes

--- src/com/epam/javatrainings/thread/Main.java
+++ src/com/epam/javatrainings/thread/Main.java
@@ -1,30 +1,38 @@
 package com.epam.javatrainings.thread;
 
 import java.util.Scanner;
+import static java.lang.Thread.sleep;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("How much time (In Minutes) ?");
-    int timet= scan.nextInt() * 60; // Convert to seconds
-    int sec=scan.nextInt();
-    long delay = timet * 100;
+    Thread countDown =
+        new Thread(
+            () -> {
+              Scanner scan = new Scanner(System.in);
+              System.out.println("How much time (In Minutes) ?");
 
-    do
-    {
-      int minutes = timet / 60;
-      int seconds = timet % 60;
-      System.out.println(minutes +" minute(s), " + seconds + " second(s)");
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      timet = timet - 1;
-      delay = delay - 100;
+              int timet = scan.nextInt() * 60; // Convert to seconds
+              System.out.println("How much time (In seconds) ?");
 
-    }
-    while (delay != 0);
-    System.out.println("Time's Up!");
-   }
+              int sec = scan.nextInt();
+              timet += sec;
+              long delay = timet * 100L;
+
+              do {
+                int minutes = timet / 60;
+                int seconds = timet % 60;
+                System.out.println(minutes + " minute(s), " + seconds + " second(s)");
+                try {
+                  sleep(1000);
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                timet = timet - 1;
+                delay = delay - 100;
+
+              } while (delay != 0);
+              System.out.println("Time's Up!");
+            });
+    countDown.start();
+  }
 }
