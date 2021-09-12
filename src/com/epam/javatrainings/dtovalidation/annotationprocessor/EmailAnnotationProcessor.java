5e777d7fdee989a71ca47564e93368ddd3b28040
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-09-12 23:31:07 +0400

    Store compiled Patern in a static final field

--- src/com/epam/javatrainings/dtovalidation/annotationprocessor/EmailAnnotationProcessor.java
+++ src/com/epam/javatrainings/dtovalidation/annotationprocessor/EmailAnnotationProcessor.java
@@ -16,13 +16,13 @@ public final class EmailAnnotationProcessor {
           + "?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2"
           + "[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-"
           + "\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+  private static final Pattern emailPattern = Pattern.compile(EMAIL_PATTERN);
 
   public static <T extends Annotation> Optional<FieldError> validate(Object val, String name, T a) {
     if (!(val instanceof String)) {
       throw new IllegalStateException("EMAIL annotation is placed on inappropriate datatype");
     }
-    Pattern p = Pattern.compile(EMAIL_PATTERN);
-    Matcher m = p.matcher((String) val);
+    Matcher m = emailPattern.matcher((String) val);
     if (m.matches()) {
       return Optional.empty();
     }
