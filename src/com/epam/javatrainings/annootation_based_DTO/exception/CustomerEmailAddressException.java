eed5761f9d0e6f9a235bf9477c5849af95af7517
Khikar-1997 <harutyunyan.xikar@bk.ru>
2021-09-14 15:59:07 +0400

    annotation based dto application

---
+++ src/com/epam/javatrainings/annootation_based_DTO/exception/CustomerEmailAddressException.java
@@ -0,0 +1,7 @@
+package com.epam.javatrainings.annootation_based_DTO.exception;
+
+public class CustomerEmailAddressException extends RuntimeException {
+    public CustomerEmailAddressException(String message) {
+        super(message);
+    }
+}
