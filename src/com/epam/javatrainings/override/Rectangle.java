f5604257f5abd4b6647729c095b02e6e718796b0
Anna Arakelyan <50499890+annaarakelyan98@users.noreply.github.com>
2021-09-15 19:01:34 +0400

    Delete src/com/epam/javatrainings/override directory

--- src/com/epam/javatrainings/override/Rectangle.java
+++
@@ -1,9 +0,0 @@
-package com.epam.javatrainings.override;
-
-public class Rectangle extends Shape {
-
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        rectangle.draw();
-    }
-}
