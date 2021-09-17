76a9e16a5325eab4880e3d521f4570862b4886f8
Sargis Sargsyan <sargiss987@gmail.com>
2021-09-17 14:53:28 +0400

    added second version of BoundedBlockingBuffer data structure

--- src/com/epam/javatrainings/multithreading/Test.java
+++ src/com/epam/javatrainings/multithreading/Test.java
@@ -3,20 +3,32 @@ package com.epam.javatrainings.multithreading;
 public class Test {
 
     public static void main(String[] args) {
-        BoundedBlockingBuffer<String> buffer = new BoundedBlockingBuffer<>();
+//        BoundedBlockingBuffer<String> buffer = new BoundedBlockingBuffer<>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            int count = i;
+//            new Thread(() -> {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                buffer.put("thread_" + count);
+//            }).start();
+//
+//            new Thread(buffer::pick).start();
+//        }
 
-        for (int i = 0; i < 10; i++) {
-            int count = i;
-            new Thread(() -> {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                buffer.put("thread_" + count);
-            }).start();
+        BoundedBlockingBuffer2<String> buffer2 = new BoundedBlockingBuffer2<>();
 
-            new Thread(buffer::pick).start();
+        for (int i = 0;i < 5;i++){
+            int count = i + 1 ;
+            new Thread(() -> buffer2.put("item " + count)).start();
         }
+
+        for (int i = 0;i < 5;i++){
+            new Thread(buffer2::take).start();
+        }
+
     }
 }
