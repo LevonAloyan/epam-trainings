a5b37240a43b6e583825959f9fad48ca78344b60
Ara Gasparyan <ara.gasparyan87@gmail.com>
2021-09-28 18:43:44 +0400

    Make Customer serializable according to tasks 4 and 5

--- src/com/epam/javatrainings/io/customers/Customer.java
+++ src/com/epam/javatrainings/io/customers/Customer.java
@@ -1,14 +1,22 @@
 package com.epam.javatrainings.io.customers;
 
-public class Customer implements Comparable<Customer> {
+import java.io.Serializable;
+
+public class Customer implements Comparable<Customer>, Serializable {
     private String name;
-    private String phone;
+    private transient String phone;
     private int score;
+    private String cardNumber;
 
     public Customer(String name, String phone, int score) {
         this.name = name;
         this.phone = phone;
         this.score = score;
+    }
+
+    public Customer(String name, String phone, int score, String cardNumber) {
+        this(name, phone, score);
+        this.cardNumber = cardNumber;
     }
 
     public String getName() {
@@ -35,12 +43,21 @@ public class Customer implements Comparable<Customer> {
         this.score = score;
     }
 
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "name='" + name + '\'' +
                 ", phone='" + phone + '\'' +
                 ", score=" + score +
+                ", cardNumber='" + cardNumber + '\'' +
                 '}';
     }
 
