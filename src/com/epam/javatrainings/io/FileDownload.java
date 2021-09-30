9530c864527530105dfa2a3107ff1b759a0dfc29
Hayk <Hayk@HAYK->
2021-09-30 07:00:20 +0400

    task 1 init

---
+++ src/com/epam/javatrainings/io/FileDownload.java
@@ -0,0 +1,69 @@
+package com.epam.javatrainings.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Hayk on 30.09.2021.
+ */
+public class FileDownload {
+
+    private FileDownload() {
+    }
+
+    public void downloadByByte(String urlAddress, String filePath) {
+        InputStream inputStream=null;
+        OutputStream outputStream=null;
+
+        try {
+            inputStream = new URL(urlAddress).openStream();
+            outputStream = new FileOutputStream(filePath);
+            int c;
+            while ((c = inputStream.read()) > 0) {
+                outputStream.write(c);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void bufferedDownload(int size,String urlAddress, String filePath){
+        InputStream inputStream=null;
+        OutputStream outputStream=null;
+
+        try {
+            inputStream = new URL(urlAddress).openStream();
+            outputStream = new FileOutputStream(filePath);
+            byte[] bytes=new byte[size];
+            while (inputStream.read(bytes)>0) {
+                outputStream.write(bytes);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/epam/javatrainings/io/IoTest.java
@@ -0,0 +1,11 @@
+package com.epam.javatrainings.io;
+
+/**
+ * Created by Hayk on 30.09.2021.
+ */
+public class IoTest {
+    public static void main(String[] args) {
+         FileDownload.bufferedDownload(4096,"http://example.com","C:\\Users\\Hayk\\Desktop\\example.html");
+         FileDownload.bufferedDownload(4096,"http://example.com","C:\\Users\\Hayk\\IdeaProjects\\epam-trainings\\src\\com\\epam\\javatrainings\\io\\example.html");
+    }
+}
